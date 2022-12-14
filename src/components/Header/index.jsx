2b847a463217f06a7de7b785813188ae91605777
Wendy Tsai <wendytsai0214@protonmail.com>
2022-12-14 16:11:37 +0800

    done with header menu animation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,39 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./style.module.scss";
-import { Button, Menu } from "antd";
+import "./styles.scss";
+import { Button, Menu, Col, Row } from "antd";
 import MenuIcon from "./icons/MenuIcon";
-import CloseOutlined from "@ant-design/icons"
+import { CloseOutlined } from "@ant-design/icons";
+import { CSSTransition } from "react-transition-group";
 const Header = () => {
-  const menuList = [
-    {
-      key: "1",
-      label: "HOME",
-    },
-    {
-      key: "2",
-      label: "ABOUT",
-    },
-    {
-      key: "3",
-      label: "PROJECTS",
-    },
-    {
-      key: "4",
-      label: "CONTACT",
-    },
-  ];
+  const [isOpen, setIsOpen] = useState();
+
+  function getItem(label, key, children) {
+    return {
+      key,
+      children,
+      label,
+    };
+  }
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
-      <div className={styles.container}>
+      <div className={`${styles.container} nav-bar`}>
         <div className={styles.logoGroup}>
-          <img className={styles.selfPhoto} src="/logo192.png"></img>
+          <img className={styles.selfPhoto} src="/images/avatar.jpg"></img>
+          <span>WENDY TSAI</span>
         </div>
         <div className={styles.menu}>
-          <MenuIcon></MenuIcon>
-          <CloseOutlined />
-          <Menu items={menuList}></Menu>
+          <Button onClick={toggleCollapse} type="text">
+            {isOpen ? (
+              <CloseOutlined style={{ fontSize: "15px" }} />
+            ) : (
+              <MenuIcon></MenuIcon>
+            )}
+          </Button>
         </div>
+      </div>
+      <div className={styles.menuList}>
+        <CSSTransition in={isOpen} classNames="dropdown" unmountOnExit>
+          <ul>
+            <li>
+              <a href="#home">HOME</a>
+            </li>
+            <li>
+              <a href="#about">ABOUT</a>
+            </li>
+            <li>
+              <a href="#projects">PROJECTS</a>
+            </li>
+            <li>
+              <a href="#contact">CONTACT</a>
+            </li>
+          </ul>
+        </CSSTransition>
       </div>
     </>
   );
